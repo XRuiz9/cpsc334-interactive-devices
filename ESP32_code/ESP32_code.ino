0121154f6b850cdc65467699f20143c1986aea53
XRuiz9 <xdruiz92@gmail.com>
2019-10-02 10:59:38 -0400

    Added joystick functionality

--- ESP32_code/ESP32_code.ino
+++ ESP32_code/ESP32_code.ino
@@ -4,22 +4,23 @@
 
 // ledPin refers to ESP32 GPIO 14
 //const int button = 14;
-const int flip = 12;
-//const int joyy = 12;
-//const int joyx = 13;
-  int analog_value = 0;
-  int analogs_value = 0;
-//int analogy_value = 0;
-//int analogx_value = 0;
+//const int flip = 12;
+//was using 17 and 18
+const int joyy = 12;
+const int joyx = 14;
+//  int analog_value = 0;
+//  int analogs_value = 0;
+int analogy_value = 0;
+int analogx_value = 0;
 
 // the setup function runs once when you press reset or power the board
 void setup() {
   // initialize digital pin ledPin as an output.
   Serial.begin(115200);
 //  pinMode(button, INPUT);
-  pinMode(flip, INPUT);
-//  pinMode(joyy, INPUT);
-//  pinMode(joyx, INPUT);
+//  pinMode(flip, INPUT);
+  pinMode(joyy, INPUT);
+  pinMode(joyx, INPUT);
   delay(1000);
 }
 
@@ -32,18 +33,17 @@ void loop() {
 //  delay(200);
 
   //Joystick
-//  analogy_value = analogRead(joyy);
-//  analogx_value = analogRead(joyx);
+  analogy_value = analogRead(joyy);
+  analogx_value = analogRead(joyx);
 
-//  Serial.println(analogy_value);
-//  Serial.println(analogx_value);
-//  Serial.println("x:" + analogx_value);
+//  Serial.println(analogy_value, DEC);
+  Serial.println(analogx_value, DEC);
 
-//  delay(100);
+  delay(200);
 
    //Switch
-   analogs_value = analogRead(flip);
-   Serial.print(analogs_value, DEC);
-   delay(200);
+//   analogs_value = analogRead(flip);
+//   Serial.print(analogs_value, DEC);
+//   delay(200);
   
 }
